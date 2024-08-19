401e429911458fefc24d056b83e951a9c65e42c5
miliincode <milena0202@icloud.com>
2024-08-19 15:18:36 +0300

    feat: created card component

---
+++ src/shared/ui/card/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const CardImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  object-position: center;
+`;
+
+export const CardTitle = styled.p`
+  font-size: 20px;
+  font-weight: 700;
+  height: 25px;
+  overflow: hidden;
+`;
+
+export const CardLink = styled.a`
+  color: #212529;
+  text-decoration: none;
+  transition: all 0.5s;
+  &:hover {
+    transition: all 0.5s;
+  }
+`;
