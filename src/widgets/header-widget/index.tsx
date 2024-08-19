45979d2c16163d8e70c4c434dcf17c62b7c92c59
miliincode <milena0202@icloud.com>
2024-08-19 13:45:42 +0300

    feat: created header-widget and added icons file

---
+++ src/widgets/header-widget/index.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from "react";
+import { ModalWindow } from "../../shared/ui/modal";
+import { Root, Image } from "./styles";
+import icon from "../../shared/icons/favicon.webp";
+
+export const HeaderWidget: FunctionComponent = () => {
+  return (
+    <Root>
+      <a href="/">
+        <Image src={icon} alt="icon" />
+      </a>
+      <ModalWindow />
+    </Root>
+  );
+};
