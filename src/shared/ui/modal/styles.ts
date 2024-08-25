8448bd7a0f0805b08e027cc00d4078a05813b698
miliincode <milena0202@icloud.com>
2024-08-25 23:19:18 +0300

    feat: changed styles

--- src/shared/ui/modal/styles.ts
+++ src/shared/ui/modal/styles.ts
@@ -1,12 +1,5 @@
 import styled from "styled-components";
 
-export const Root = styled.div`
-  margin: 2%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
 export const Image = styled.img`
   width: 50px;
   height: 50px;
