fd689c8c80d4f90dd0eecea54c5fa6246734f613
miliincode <milena0202@icloud.com>
2024-08-28 22:49:19 +0300

    feat: added modal window widget

--- src/widgets/header-widget/index.tsx
+++ src/widgets/header-widget/index.tsx
@@ -1,21 +1,77 @@
 import React, { FunctionComponent } from "react";
-import {
-  Button
-} from "reactstrap";
-import { Root, Image, Buttons } from "./styles";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { schema } from "./schema";
+import { ModalWindow } from "../../shared/ui/modal";
+import { InputWidget } from "../../shared/ui/input";
+import { Root, Image, Buttons, Inputs } from "./styles";
 import icon from "../../shared/icons/favicon.webp";
 
 export const HeaderWidget: FunctionComponent = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm<yup.InferType<typeof schema>>({
+    resolver: yupResolver(schema),
+  });
+
+  const testArray: Array<any> = [
+    {
+      placeholder: "Введите имя",
+      register: register("name"),
+      error: errors.name,
+      message: errors.name?.message?.toString(),
+      description: "Укажите название вашей новости",
+    },
+  ];
+
   return (
     <Root>
       <a href="/">
         <Image src={icon} alt="icon" />
       </a>
       <Buttons>
-        <a href="/">Вход</a>
-        <Button color="primary" onClick={() => {}}>
-          Регистрация
-        </Button>
+        <ModalWindow
+          modalButtonName="Вход"
+          modalTitle="Вход"
+          handlerAdd={() => {}}
+          handlerModalOpen={() => {}}
+          toggleModal={() => {}}
+          form={
+            <Inputs>
+              {testArray.map((el) => (
+                <InputWidget
+                  placeholder={el.placeholder}
+                  register={el.register}
+                  error={el.error}
+                  message={el.message}
+                  description={el.description}
+                />
+              ))}
+            </Inputs>
+          }
+        />
+        <ModalWindow
+          modalButtonName="Регистрация"
+          modalTitle="Регистрация"
+          handlerAdd={() => {}}
+          handlerModalOpen={() => {}}
+          toggleModal={() => {}}
+          form={
+            <Inputs>
+              {testArray.map((el) => (
+                <InputWidget
+                  placeholder={el.placeholder}
+                  register={el.register}
+                  error={el.error}
+                  message={el.message}
+                  description={el.description}
+                />
+              ))}
+            </Inputs>
+          }
+        />
       </Buttons>
     </Root>
   );
