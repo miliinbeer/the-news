3c6194ce34c324218c50886db2ee8407863e48a6
miliincode <milena0202@icloud.com>
2024-08-30 17:31:19 +0300

    feat: changed props and imports

--- src/widgets/header-widget/index.tsx
+++ src/widgets/header-widget/index.tsx
@@ -1,16 +1,24 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { schema } from "./schema";
 import { ModalWindow } from "../../shared/ui/modal";
 import { Input } from "../../shared/ui/input";
+import { Button } from "reactstrap";
 import { Root, Image, Buttons, Inputs } from "./styles";
+import {
+  Descriptions,
+  ErrorMessage,
+  Description,
+} from "../../shared/ui/input/styles";
 import icon from "../../shared/icons/favicon.webp";
 
 export const HeaderWidget: FunctionComponent = () => {
   const {
+    reset,
     register,
+    handleSubmit,
     formState: { errors },
   } = useForm<yup.InferType<typeof schema>>({
     resolver: yupResolver(schema),
@@ -22,9 +30,15 @@ export const HeaderWidget: FunctionComponent = () => {
       register: register("name"),
       error: errors.name,
       message: errors.name?.message?.toString(),
-      description: "Укажите название вашей новости",
+      description: "Введите ваше имя",
     },
   ];
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+    reset();
+  };
 
   return (
     <Root>
@@ -33,39 +47,79 @@ export const HeaderWidget: FunctionComponent = () => {
       </a>
       <Buttons>
         <ModalWindow
+          buttonVariant="link"
+          handlerModalOpen={toggleModal}
           modalButtonName="Вход"
-          modalTitle="Вход"
+          isOpened={modal}
+          toggleModal={toggleModal}
+          modalTitle="Укажите ваше имя"
           handlerAdd={() => {}}
-          handlerModalOpen={() => {}}
-          toggleModal={() => {}}
-          form={
-            <Inputs>
+          modalForm={
+            <>
               {testArray.map((el) => (
                 <Input
                   placeholder={el.placeholder}
                   register={el.register}
-                  descriptions={<></>}
+                  descriptions={
+                    <Descriptions>
+                      {el.error ? (
+                        <ErrorMessage>{el.message}</ErrorMessage>
+                      ) : (
+                        <Description>{el.description}</Description>
+                      )}
+                    </Descriptions>
+                  }
                 />
               ))}
-            </Inputs>
+            </>
+          }
+          modalButtons={
+            <>
+              <Button color="primary" onClick={() => {}}>
+                Добавить
+              </Button>
+              <Button color="secondary" onClick={toggleModal}>
+                Отмена
+              </Button>
+            </>
           }
         />
         <ModalWindow
+          buttonVariant="primary"
+          handlerModalOpen={toggleModal}
           modalButtonName="Регистрация"
-          modalTitle="Регистрация"
+          isOpened={modal}
+          toggleModal={toggleModal}
+          modalTitle="Укажите ваше имя"
           handlerAdd={() => {}}
-          handlerModalOpen={() => {}}
-          toggleModal={() => {}}
-          form={
-            <Inputs>
+          modalForm={
+            <>
               {testArray.map((el) => (
                 <Input
                   placeholder={el.placeholder}
                   register={el.register}
-                  descriptions={<></>}
+                  descriptions={
+                    <Descriptions>
+                      {el.error ? (
+                        <ErrorMessage>{el.message}</ErrorMessage>
+                      ) : (
+                        <Description>{el.description}</Description>
+                      )}
+                    </Descriptions>
+                  }
                 />
               ))}
-            </Inputs>
+            </>
+          }
+          modalButtons={
+            <>
+              <Button color="primary" onClick={() => {}}>
+                Добавить
+              </Button>
+              <Button color="secondary" onClick={toggleModal}>
+                Отмена
+              </Button>
+            </>
           }
         />
       </Buttons>
